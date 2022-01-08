3149f25bf2e3ddd08a911c833c0823885fe12791
Rosemary91 <rosebel.duah47@gmail.com>
2022-01-08 09:06:51 -0800

    update in main.js

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-require('dotenv').config({path: '.env'});
+require('dotenv').config();
 const express = require('express');
 const server = express();
 const mongoose = require('mongoose');
@@ -9,7 +9,7 @@ const port = process.env.PORT || 6999;
 server.listen(port,function(){
     console.log('Periserver connected...');
 
-    mongoose.connect(process.env.ATLAS_URL,{useNewUrlParser:true,useUnifiedTopology:true})
+    mongoose.connect(process.env.ATLAS_URL)
     .then(function(){
         console.log('ATLAS CONNECTED...');
         server.use(express.json());
